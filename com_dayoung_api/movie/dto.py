67470f59a99714b6bb60c4a8339b6a89164c0d66
Beomsudev <bumsu@naver.com>
2020-10-21 16:57:19 +0900

    1021-2

--- com_dayoung_api/movie/dto.py
+++ com_dayoung_api/movie/dto.py
@@ -1,57 +1,84 @@
 from com_dayoung_api.ext.db import db
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine
+from com_dayoung_api.movie.service import MovieService
+
+
+from com_dayoung_api.utils.file_helper import FileReader
+
+config = {
+    'user' : 'root',
+    'password' : 'root',
+    'host': '127.0.0.1',
+    'port' : '3306',
+    'database' : 'dayoungdb'
+}
+charset = {'utf8':'utf8'}
+url = f"mysql+mysqlconnector://{config['user']}:{config['password']}@{config['host']}:{config['port']}/{config['database']}?charset=utf8"
+engine = create_engine(url)
 
 class MovieDto(db.Model):
     
     __tablename__ = 'movies'
     __table_args__= {'mysql_collate':'utf8_general_ci'}
 
-    id : int = db.Column(db.Integer, primary_key = True, index = True)
-    movie_name : str = db.Column(db.String(30))
+    id : str = db.Column(db.String(30), primary_key = True, index = True)
+    title : str = db.Column(db.String(30))
     genre : str = db.Column(db.String(30))
     country : str = db.Column(db.String(30))
     year : str = db.Column(db.String(4))
     company : str = db.Column(db.String(30))
     director : str = db.Column(db.String(30))
     actor : str = db.Column(db.String(30))
+    date : str = db.Column(db.String(30))
+    running_time : str = db.Column(db.String(30))
     keyword : str = db.Column(db.String(30))
+    plot : str = db.Column(db.String(30))
 
-    articles = db.relationship('ArticleModel', lazy='dynamic')
-
-    def __init__(self, id, movie_name, genre, country, year, company, director, actor, keyword):
+    def __init__(self, id, title, genre, country, year, company, director, actor, date, running_time, keyword, plot):
         self.id = id
-        self.movie_name = movie_name
+        self.title = title
         self.genre = genre
         self.country = country
         self.year = year
         self.company = company
         self.director = director
         self.actor = actor
+        self.date = date
+        self.running_time = running_time
         self.keyword = keyword
+        self.plot = plot
 
 
-    def __repr__(self):
-        return f'Movie(id=\'{self.id}\',\
-            movie_name=\'{self.movie_name}\',\
-            genre=\'{self.genre}\',\
-            country=\'{self.country}\',\
-            year=\'{self.year}\',\
-            company=\'{self.company}\',\
-            director=\'{self.director}\',\
-            actor=\'{self.actor}\',\
-            keyword=\'{self.keyword}\',)'
+    # def __repr__(self):
+    #     return f'Movie(id=\'{self.id}\',\
+    #         title=\'{self.title}\',\
+    #         genre=\'{self.genre}\',\
+    #         country=\'{self.country}\',\
+    #         year=\'{self.year}\',\
+    #         company=\'{self.company}\',\
+    #         director=\'{self.director}\',\
+    #         actor=\'{self.actor}\',\
+    #         date=\'{self.date}\',\
+    #         running_time=\'{self.running_time}\',\
+    #         keyword=\'{self.keyword}\',\
+    #         plot=\'{self.plot}\',)'
 
     @property
     def json(self):
         return {
             'id' : self.id,
-            'movie_name' : self.movie_name,
+            'title' : self.title,
             'genre' : self.genre,
             'country' : self.country,
             'year' : self.year,
             'company' : self.company,
             'director' : self.director,
             'actor' : self.actor,
-            'keyword' : self.keyword
+            'date' : self.date,
+            'running_time' : self.running_time,
+            'keyword' : self.keyword,
+            'plot' : self.plot
         }
 
     def save(self):
@@ -61,3 +88,14 @@ class MovieDto(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+
+
+service = MovieService()
+Session = sessionmaker(bind=engine)
+s = Session()
+df = service.hook()
+print(df.head())
+s.bulk_insert_mappings(MovieDto, df.to_dict(orient="records"))
+s.commit()
+s.close()
