993233424812e9638c756703122cb5492caf18f9
Beomsudev <bumsu@naver.com>
2020-10-23 15:41:15 +0900

    1023-1

--- com_dayoung_api/movie/dto.py
+++ com_dayoung_api/movie/dto.py
@@ -1,8 +1,8 @@
 from com_dayoung_api.ext.db import db
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import create_engine
-from com_dayoung_api.movie.service import MovieService
-
+from com_dayoung_api.movie.search import NaverMovie
+import pandas as pd
 from com_dayoung_api.utils.file_helper import FileReader
 
 config = {
@@ -20,64 +20,48 @@ class MovieDto(db.Model):
     
     __tablename__ = 'movies'
     __table_args__= {'mysql_collate':'utf8_general_ci'}
-
-    id : str = db.Column(db.String(30), primary_key = True, index = True)
+    # ,id,title,subtitle,description,imageurl,year,rating
+    id : str = db.Column(db.String(10), primary_key = True, index = True)
     title : str = db.Column(db.String(30))
-    genre : str = db.Column(db.String(30))
-    country : str = db.Column(db.String(30))
-    year : str = db.Column(db.String(4))
-    company : str = db.Column(db.String(30))
-    director : str = db.Column(db.String(30))
-    actor : str = db.Column(db.String(30))
-    date : str = db.Column(db.String(30))
-    running_time : str = db.Column(db.String(30))
-    keyword : str = db.Column(db.String(30))
-    plot : str = db.Column(db.String(30))
+    subtitle : str = db.Column(db.String(30))
+    description : str = db.Column(db.String(200))
+    imageurl : str = db.Column(db.String(100))
+    year : str = db.Column(db.String(5))
+    rating : float = db.Column(db.Float)
 
-    def __init__(self, id, title, genre, country, year, compacompanyny, director, actor, date, running_time, keyword, plot):
+    def __init__(self,id,title,subtitle,description,imageurl,year,rating):
         self.id = id
         self.title = title
-        self.genre = genre
-        self.country = country
+        self.subtitle = subtitle
+        self.description = description
+        self.imageurl = imageurl
         self.year = year
-        self.company = company
-        self.director = director
-        self.actor = actor
-        self.date = date
-        self.running_time = running_time
-        self.keyword = keyword
-        self.plot = plot
+        self.rating = rating
 
-
-    def __repr__(self):
-        return f'Movie(id=\'{self.id}\',\
-            title=\'{self.title}\',\
-            genre=\'{self.genre}\',\
-            country=\'{self.country}\',\
-            year=\'{self.year}\',\
-            company=\'{self.company}\',\
-            director=\'{self.director}\',\
-            actor=\'{self.actor}\',\
-            date=\'{self.date}\',\
-            running_time=\'{self.running_time}\',\
-            keyword=\'{self.keyword}\',\
-            plot=\'{self.plot}\',)'
+    # def __repr__(self):
+    #     return f'Movie(id=\'{self.id}\',\
+    #         title=\'{self.title}\',\
+    #         genre=\'{self.genre}\',\
+    #         country=\'{self.country}\',\
+    #         year=\'{self.year}\',\
+    #         company=\'{self.company}\',\
+    #         director=\'{self.director}\',\
+    #         actor=\'{self.actor}\',\
+    #         date=\'{self.date}\',\
+    #         running_time=\'{self.running_time}\',\
+    #         keyword=\'{self.keyword}\',\
+    #         plot=\'{self.plot}\',)'
 
     @property
     def json(self):
         return {
             'id' : self.id,
             'title' : self.title,
-            'genre' : self.genre,
-            'country' : self.country,
+            'subtitle' : self.subtitle,
+            'description' : self.description,
+            'imageurl' : self.imageurl,
             'year' : self.year,
-            'company' : self.company,
-            'director' : self.director,
-            'actor' : self.actor,
-            'date' : self.date,
-            'running_time' : self.running_time,
-            'keyword' : self.keyword,
-            'plot' : self.plot
+            'rating' : self.rating
         }
 
     def save(self):
@@ -88,10 +72,10 @@ class MovieDto(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-service = MovieService()
+search = NaverMovie()
 Session = sessionmaker(bind=engine)
 s = Session()
-df = service.hook()
+df = search.hook()
 print(df.head())
 s.bulk_insert_mappings(MovieDto, df.to_dict(orient="records"))
 s.commit()
